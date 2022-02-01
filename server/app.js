935ac145278733d213c8d97678dd0f5c86652762
Rufat <75755323+Rufat00@users.noreply.github.com>
2022-02-01 23:43:49 +0400

    cors aded

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const mongo = require('mongoose')
+const cors = require('cors')
 
 const app =  express()
 app.use(express.json())
+app.use(cors())
 
 app.use('/auth', require('./auth.route.js'))
 app.use('/todo', require('./todos.route.js'))
